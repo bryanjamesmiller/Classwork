fe15f4947f09e8c31447bf8becd6c86884b71ac6
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-09-19 15:32:38 -0500

    making progress again.......?

--- Bryans Java Workspace/CS22_Assignment 1/src/MagicSquare.java
+++ Bryans Java Workspace/CS22_Assignment 1/src/MagicSquare.java
@@ -9,12 +9,25 @@
 import java.util.*;
 
 public class MagicSquare {
-    // the current contents of the cells of the puzzle values[r][c]
+	//Starting row and column values
+    private static final int FIRST_COLUMN = 0;
+	private static final int FIRST_ROW = 0;
+	
+	// The minimum possible number to try to put on the magic square
+	private static final int MIN_NUM = 1;
+
+	// the current contents of the cells of the puzzle values[r][c]
     // gives the value in the cell at row r, column c
     private int[][] values;
 
     // the order (i.e., the dimension) of the puzzle
     private int order;
+    
+    //The maximum possible number to try putting on the magic square
+    private int maxNum;
+    
+    // Whether or not the puzzle is solved
+    private boolean isSolved=false;
 
     /**
      * Creates a MagicSquare object for a puzzle with the specified
@@ -23,7 +36,7 @@ public class MagicSquare {
     public MagicSquare(int order) {
         values = new int[order][order];
         this.order = order;
-
+        this.maxNum=this.order*this.order;
         // Add code to this constructor as needed to initialize
         // the fields that you add to the object.
     }
@@ -37,8 +50,35 @@ public class MagicSquare {
         // Replace the line below with your implementation of this method.
         // REMEMBER: The recursive-backtracking code should NOT go here.
         // See the comments above.
-        return false;
+    	fillOneMagicSquare(FIRST_ROW, FIRST_COLUMN);
+  
+    	return true;
+//      return isSolved;
     }
+    
+    public void fillOneMagicSquare(int row, int col){
+    	if(row == this.order && col == this.order){
+    		this.isSolved=true;
+    		System.out.println("Made it to the base case");
+    		return;    		
+    	}
+    	
+    	for(int i=MIN_NUM; i<=this.maxNum; i++){
+    		System.out.println("Row is " + row);
+    		System.out.println("Col is " + col);
+    		System.out.println("i is " + col);
+    		System.out.println();
+    		values[row][col]=i;    		
+    		
+    		if(col<this.order){
+    			System.out.println("Made it to the if and i=" + i);
+    			fillOneMagicSquare(row, col+1);
+    		}
+    		values[row][col]=i;	
+    		fillOneMagicSquare(row+1, FIRST_COLUMN);
+    	}    	
+    }
+    
 
     /**
      * Displays the current state of the puzzle.
