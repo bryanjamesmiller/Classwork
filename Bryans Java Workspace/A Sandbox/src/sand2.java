5d225b26b9d22a0aac6e381ebaef1bfe094beacf
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-10-01 17:29:00 -0500

    taa

--- Bryans Java Workspace/A Sandbox/src/sand2.java
+++ Bryans Java Workspace/A Sandbox/src/sand2.java
@@ -1,49 +1,37 @@
 
 public class sand2 {
-
-	public static int[][] values = new int[3][3];
-
-	public static boolean[] isNumberAvailable = new boolean[9];    
-	public static boolean[] isNumberAvailableTemp = new boolean[9];    
-
-	public static void main(String [] args) {
-
-
-		for(int i=0; i<3*3; i++)
-			isNumberAvailableTemp[i]=true;
-
-		for(int i=0; i<3*3; i++)
-			isNumberAvailable[i]=true;
-
-		for (int row = 0; row < 3; row++) 
-		{
-			for (int col = 0; col < 3; col++) 
-				values[row][col] = 0;  
-		}
-
-		int testSum=0;
-
-		//The below is like a recursive call with makeAValidMagicSquaresRow(row+1) each time
-		makeAValidMagicSquaresRow(0);
+	public static void main(String[] args){
+		int[] myArray = {10, 18, 4, 24, 33, 40, 8, 3, 12};
+		insertionSort(myArray);
+		for(int i=0; i<myArray.length; i++)
+			System.out.print(myArray[i] + ", ");
 	}
 
-	public static void makeAValidMagicSquaresRow(int row){
+	public static void insertionSort(int[] arr) {
 
-		int testSum=0;
-
-		for(int i=0; i<3; i++)
+		for (int i = 1; i < 9; i++) 
 		{
-			while(values[row][i]==0)
+			System.out.println("\n" + i);
+			//Each "for" loop compares ONLY the first two elements, i and i-1
+			//If arr[i] < arr[i-1], then it puts arr[i] at arr[i-1] and shifts everything else over to the right one space.
+			//So the first loop doesn't do anything because 10<18
+			//the second loop sees 18 and 4, so it puts 4 at the left and shifts everything else to the right one space
+			//the third loop doesn't do anything because 18<24
+			//the fourth loop sees 24 and 33, so it doesn't do anything either
+			if (arr[i] < arr[i-1]) 
 			{
-				int testNum = 1 + (int)((9-1+1) * Math.random());
-				if(isNumberAvailableTemp[testNum-1]==true)
-				{
-					values[row][i]=testNum;
-					isNumberAvailableTemp[testNum-1]=false;
-					testSum+=testNum;
-				}
-			} 
-			System.out.println("When is this zero: " + values[row][i]);
+				int toInsert = arr[i];
+
+				int j = i;
+				do {
+					arr[j] = arr[j-1];
+					j = j - 1;
+				} while (j > 0 && toInsert < arr[j-1]);
+				arr[j] = toInsert;
+			}
+			for(int j=0; j<arr.length; j++)
+				System.out.print(arr[j] + ", ");
 		}
+		System.out.println("\n");
 	}
 }
