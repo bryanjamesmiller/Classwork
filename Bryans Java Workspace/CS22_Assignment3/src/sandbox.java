9075178d449d4c9746bace0679d66754a81c9057
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-10-10 23:34:16 -0500

    n. 1 and 3 done!

--- Bryans Java Workspace/CS22_Assignment3/src/sandbox.java
+++ Bryans Java Workspace/CS22_Assignment3/src/sandbox.java
@@ -8,6 +8,13 @@ public class sandbox
 		private IntNode next;
 	}
 
+	public class DoubleNode 
+	{
+		private int ch;
+		private DoubleNode next;
+		private DoubleNode prev;
+	}
+
 	public void printOddsRecursive(IntNode str)
 	{
 		if(str == null)
@@ -33,9 +40,30 @@ public class sandbox
 	}
 
 
-	public static IntNode removeAllOccurrences(IntNode str, char ch)
+	public static DoubleNode removeAllOccurrences(DoubleNode str, char ch)
 	{
+		if (str == null)
+			throw new IllegalArgumentException("string is empty");
 
+		DoubleNode tempStr=str; //save the pointer to the whole list and update it if the first (several?) items match ch and get deleted
+		
+		while(str != null)
+		{
+			if (str.ch == ch && str.prev == null) //If the first node has the ch we're looking to delete, delete it by:
+			{
+				str.next.prev = null;  //Take out the next node's "prev" reference to it
+				tempStr = str.next;  // Skip it out and make the reference point to the new first item in the list
+			}
+			else if (str.ch == ch) //If it's not the first node and it has the ch we're looking to delete, delete it by:
+			{
+				str.prev.next = str.next;
+				str.next.prev = str.prev;
+				//Notice str still points to str.next also, so str = str.next; should be fine 
+			}			
+			str = str.next;  			
+		}
+
+		return tempStr;  //Return the reference to the first DoubleNode
 	}
 }
 
