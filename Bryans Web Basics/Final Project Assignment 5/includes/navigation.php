729ffbfc9ccde543d7cddbf27c6cb235866a9551
Bryan Miller <gratiafide@yahoo.com>
2014-12-23 09:19:19 -0600

    links work

--- Bryans Web Basics/Final Project Assignment 5/includes/navigation.php
+++ Bryans Web Basics/Final Project Assignment 5/includes/navigation.php
@@ -1,9 +1,9 @@
 <nav id="navigation">
     <ul id="category">
         <li>Website Links
-            <ul><li id="nav-1"><a href="/index.php">Home</a></li>
-                <li id="nav-2"><a href="/spurs.php">San Antonio Spurs</a></li>
-                <li id="nav-3"><a href="/rockets.php">Houston Rockets</a></li>
+            <ul><li id="nav-1"><a href="index.php">Home</a></li>
+                <li id="nav-2"><a href="spurs.php">San Antonio Spurs</a></li>
+                <li id="nav-3"><a href="rockets.php">Houston Rockets</a></li>
                 <li><a href="#">Portland Trailblazers</a></li>
                 <li><a href="#">LA Clippers</a></li>
                 <li><a href="#">Golden State Warriors</a></li>
