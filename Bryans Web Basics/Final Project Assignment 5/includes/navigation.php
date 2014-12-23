a218002d10b36d00b3361d1b1f9a7002f558b478
Bryan Miller <gratiafide@yahoo.com>
2014-12-23 09:26:04 -0600

    link fix

--- Bryans Web Basics/Final Project Assignment 5/includes/navigation.php
+++ Bryans Web Basics/Final Project Assignment 5/includes/navigation.php
@@ -4,20 +4,20 @@
             <ul><li id="nav-1"><a href="index.php">Home</a></li>
                 <li id="nav-2"><a href="spurs.php">San Antonio Spurs</a></li>
                 <li id="nav-3"><a href="rockets.php">Houston Rockets</a></li>
-                <li><a href="#">Portland Trailblazers</a></li>
-                <li><a href="#">LA Clippers</a></li>
-                <li><a href="#">Golden State Warriors</a></li>
-                <li><a href="#">Dallas Mavericks</a></li>
-                <li><a href="#">Washington Wizards</a></li>
-                <li><a href="#">Miami Heat</a></li>
-                <li><a href="#">Golden State Warriors</a></li>
-                <li><a href="#">Indiana Pacers</a></li>
-                <li><a href="#">Brooklyn Nets</a></li>
-                <li><a href="#">Toronto Raptors</a></li>
-                <li><a href="#">Chicago Bulls</a></li>
-                <li><a href="#">Charlotte Bobcats</a></li>
-                <li><a href="#">Atlanta Hawks</a></li>
-                <li><a href="#">Memphis Grizzlies</a></li>
+                <li><a href="soon.php">Portland Trailblazers</a></li>
+                <li><a href="soon.php">LA Clippers</a></li>
+                <li><a href="soon.php">Golden State Warriors</a></li>
+                <li><a href="soon.php">Dallas Mavericks</a></li>
+                <li><a href="soon.php">Washington Wizards</a></li>
+                <li><a href="soon.php">Miami Heat</a></li>
+                <li><a href="soon.php">Golden State Warriors</a></li>
+                <li><a href="soon.php">Indiana Pacers</a></li>
+                <li><a href="soon.php">Brooklyn Nets</a></li>
+                <li><a href="soon.php">Toronto Raptors</a></li>
+                <li><a href="soon.php">Chicago Bulls</a></li>
+                <li><a href="soon.php">Charlotte Bobcats</a></li>
+                <li><a href="soon.php">Atlanta Hawks</a></li>
+                <li><a href="soon.php">Memphis Grizzlies</a></li>
             </ul>
         </li>
     </ul>
---
+++ Bryans Web Basics/Final Project Assignment 5/soon.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Rockets 2013-2014 Season</title>
+    <meta  charset="utf-8" />
+    <link rel="stylesheet" href="css/base_style.css" type="text/css" />
+    <link rel="stylesheet" href="css/rockets_style.css" type="text/css" />
+    <script type="text/javascript" src="js/jwplayer/jwplayer.js"></script>
+    <script type="text/javascript"> jwplayer.key="98l+M93QyvmhWC3lvJh1bloOwBIASEs9DzjQow==";</script>
+</head>
+<body>
+<?php include ("includes/navigation.php"); ?>
+<h2>Coming Next Season!</h2>
+<hr class="line">
+<div id="videoHere">Loading...</div>
+<script type="text/javascript">
+    jwplayer("videoHere").setup({
+        file: "http://youtu.be/LIvxdl8LDwk",
+        height: 600,
+        width: 800,
+    });
+</script>
+<hr class="line">
+
+</body>
+</html>
