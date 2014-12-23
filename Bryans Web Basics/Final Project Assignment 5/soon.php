570bd691422a445b8488dbfb01f84efddc4d4fc5
Bryan Miller <gratiafide@yahoo.com>
2014-12-23 09:34:05 -0600

    links

--- Bryans Web Basics/Final Project Assignment 5/soon.php
+++ Bryans Web Basics/Final Project Assignment 5/soon.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Rockets 2013-2014 Season</title>
+    <title>Next Season</title>
     <meta  charset="utf-8" />
     <link rel="stylesheet" href="css/base_style.css" type="text/css" />
-    <link rel="stylesheet" href="css/rockets_style.css" type="text/css" />
+    <link rel="stylesheet" href="css/index_style.css" type="text/css" />
     <script type="text/javascript" src="js/jwplayer/jwplayer.js"></script>
     <script type="text/javascript"> jwplayer.key="98l+M93QyvmhWC3lvJh1bloOwBIASEs9DzjQow==";</script>
 </head>
@@ -12,18 +12,18 @@
 <h1>Coming Next Season!</h1>
 <span>
     <hr class="line">
-<div id="videoHere">Loading...</div>
+<span id="videoHere">Loading...</span>
 <script type="text/javascript">
     jwplayer("videoHere").setup({
         file: "http://youtu.be/LIvxdl8LDwk",
         height: 600,
-        width: 800,
+        width: 800
     });
 </script>
 <hr class="line">
 </span>
 
-<span><?php include ("includes/navigation.php"); ?></span>
+<span id="right_justify"><?php include ("includes/navigation.php"); ?></span>
 
 </body>
 </html>
